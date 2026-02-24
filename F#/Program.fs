4b1477080e32d632579ffe62035043c9f0838c87
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-02-24 15:16:26 +0500

    1

---
+++ F#/Program.fs
@@ -0,0 +1,18 @@
+﻿open System //библиотека
+
+let listEmpty = [] //список
+let Spisok = 
+    [
+        while
+        printf "Введите число: "
+        let n = int(Console.ReadLine())
+    
+        1
+    ]
+    
+[<EntryPoint>] //точка входа
+let main _ =
+    //Spisok 
+    printfn "Список завершён"
+    printfn "Вывело: " Spisok
+    0 // то же что и return 0
